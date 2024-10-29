1f5d1748482178f17a02aaabe32ce1aff416c2c7
MJMahedi <mdrokibulhasan5321@gmail.com>
2024-10-29 07:11:55 +0600

    product details page responsive done

--- src/Components/products/Product.jsx
+++ src/Components/products/Product.jsx
@@ -37,60 +37,90 @@ function Product() {
 
   return (
     <div>
-      <main className="py-10 bg-sky-50">
-        <div className="container space-x-4 mx-auto px-4 flex flex-col md:flex-row ">
+      <main className="py-3 lg:py-6 bg-sky-50">
+        <div className="container  mx-auto px-4 flex flex-col md:flex-row">
 
-          <div className="w-3/4">
-            <div className="grid grid-cols-2 gap-8">
+          {/* Left Section - Image Previews Only For Large device */}
+          <div className="hidden lg:block w-full lg:w-3/4">
+            <div className="grid lg:grid-cols-2 gap-8 relative">
 
-
-              <div className='grid grid-cols-3 gap-6'>
+              {/* All Image Preview Section */}
+              <div className='grid gap-3 lg:grid-cols-3'>
                 {product.image.map((image, index) => (
-                  <div key={index} onClick={() => setSelectedImage(image)} // Update selected image on click
+                  <div
+                    key={index}
+                    onClick={() => setSelectedImage(image)} // Update selected image on click
                     style={{
                       backgroundImage: `url(${image})`,
                       backgroundSize: `cover`,
-                      // backgroundRepeat: `no-repeat`,
                       backgroundPosition: `center`
-                    }} className=' aspect-[3/4] rounded-md shadow-md cursor-pointer'>
+                    }}
+                    className='aspect-[3/4] w-28 h-40 rounded-md shadow-md cursor-pointer z-20'
+                  >
                   </div>
                 ))}
               </div>
-              {/* <div className='flex justify-center space-x-5 mt-4'>
-                {product.size.map((size, index) => (
-                  <div key={index} style={{
-                    backgroundImage: `url(${product.image})`,
-                    backgroundSize: `cover`,
-                    backgroundRepeat: `no-repeat`,
-                    backgroundPosition: `center`
-                  }} className='w-28 h-20 object-cover rounded-md shadow-md'>
+
+              {/* Right Side - Main Image Preview */}
+              <div
+                style={{
+                  backgroundImage: `url(${selectedImage})`,
+                  backgroundSize: `cover`,
+                  backgroundPosition: `center`,
+                  width: '300px',  // Custom width
+                  height: '400px'   // Custom height
+                }}
+                className="flex justify-center items-center w-full max-w-sm aspect-[3/4] rounded-lg shadow-lg z-10"
+              >
+              </div>
+            </div>
+          </div>
+          {/* Left Section - Image Previews Only For Small & Medium device */}
+          <div className="block lg:hidden w-full md:w-1/2 px-4">
+            <div className="flex flex-wrap justify-around		">
+              {/* Main Image Preview */}
+              <div
+                style={{
+                  backgroundImage: `url(${selectedImage})`,
+                  backgroundSize: `cover`,
+                  backgroundPosition: `center`,
+                  width: '300px',  // Custom width
+                  height: '400px'   // Custom height
+                }}
+                className="flex justify-center items-center  pl-0  max-w-sm aspect-[3/4] rounded-lg shadow-lg z-10"
+              >
+              </div>
+              {/* All Image Preview Section */}
+              <div className='flex flex-row gap-2 md:gap-4 items-center ml-0 z-10'>
+                {product.image.map((image, index) => (
+                  <div
+                    key={index}
+                    onClick={() => setSelectedImage(image)} // Update selected image on click
+                    style={{
+                      backgroundImage: `url(${image})`,
+                      backgroundSize: `cover`,
+                      backgroundPosition: `center`
+                    }}
+                    className='my-5 aspect-[3/4] w-15 h-20 md:w-27 md:h-36 rounded-md shadow-md cursor-pointer'
+                  >
                   </div>
                 ))}
-              </div> */}
-               {/* Right Side - Main Image Preview */}
-              <div style={{
-                backgroundImage: `url(${selectedImage})`,
-                backgroundSize: `cover`,
-                backgroundRepeat: `no-repeat`,
-                backgroundPosition: `center`,
-                width: '300px',  // ichcha moto width
-                height: '400px'   // ichcha moto height
-              }} className="flex justify-center items-center w-full max-w-sm aspect-[3/4] rounded-lg shadow-lg">
               </div>
             </div>
           </div>
 
-          <div className="w-1/4 md:pl-0 mt-0">
+          {/* Right Section - Product Details */}
+          <div className="w-full md:w-1/2  mt-0">
             <h1 className="text-xl font-bold text-gray-900">{product.title}</h1>
 
             <div className='p-2 bg-yellow-200 rounded-2xl'>
-              <p className="text-md text-gray-700 font-semibold">Original Price : {product.price} Tk</p>
-              <p className="text-sm text-gray-600 cursor-auto font-semibold">Discount : {product.discount} Tk</p>
+              <p className="text-md text-gray-700 font-semibold">Original Price: {product.price} Tk</p>
+              <p className="text-sm text-gray-600 cursor-auto font-semibold">Discount: {product.discount} Tk</p>
             </div>
 
-            <p className="text-sm text-gray-600 mt-4">{product.description}</p>
+            <p className=" text-sm text-gray-600 mt-2">{product.description}</p>
 
-            <div className="mt-4 text-sm">
+            <div className="mt-2 text-sm">
               <label htmlFor="size" className="block text-gray-700 font-semibold">Size</label>
               <select id="size" className="block w-full mt-2 p-2 border border-gray-300 rounded-lg">
                 {product.size.map((size, index) => (
@@ -99,7 +129,7 @@ function Product() {
               </select>
             </div>
 
-            <div className="mt-4 text-sm">
+            <div className="mt-2 text-sm">
               <label htmlFor="color" className="block text-gray-700 font-semibold">Color</label>
               <div className="flex space-x-4 mt-2">
                 {product.color.map((color, index) => (
@@ -109,14 +139,14 @@ function Product() {
             </div>
 
             <Link to={"/shoping-cart"} onClick={addToBasket}>
-              <button className="w-full mt-10 animate-bounce bg-gradient-to-tr from-sky-900 to-blue-500 text-white py-[10px] font-bold px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600">
+              <button className="w-[100%] mt-4 animate-bounce bg-gradient-to-tr from-sky-900 to-blue-500 text-white py-[10px] font-bold px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 ">
                 <p className='animate-pulse tracking-widest'>Buy Now</p>
               </button>
             </Link>
-
           </div>
         </div>
 
+        {/* Product Description */}
         <div className="container mx-auto px-4 mt-12 text-sm">
           <div className="bg-gradient-to-tr from-sky-50 to-sky-600 shadow-lg rounded-lg p-6">
             <h2 className="text-xl font-bold text-gray-900">Product Description</h2>
@@ -139,6 +169,7 @@ function Product() {
         </div>
       </main>
     </div>
+
   );
 }
 
